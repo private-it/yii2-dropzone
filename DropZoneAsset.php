1084b8b2159afd344898a527dfd7428bafa49b2c
Rendol <sapegin@private-it.ru>
2017-02-05 15:45:33 +0700

    add debug mode for assets

--- DropZoneAsset.php
+++ DropZoneAsset.php
@@ -8,13 +8,13 @@ class DropZoneAsset extends AssetBundle
 {
     public $sourcePath = '@bower/dropzone/dist';
     public $css = [
-        'min/dropzone.min.css',
+        YII_DEBUG ? 'dropzone.css' : 'min/dropzone.min.css',
     ];
     public $depends = [
         'yii\web\JqueryAsset',
         'yii\jui\JuiAsset',
     ];
     public $js = [
-        'min/dropzone.min.js',
+        YII_DEBUG ? 'dropzone.js' : 'min/dropzone.min.js',
     ];
 }
